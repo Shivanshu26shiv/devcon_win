e575c2facbda9936e6d4a1fa1d2733cb93a6f3b3
Shivanshu Srivastava <shivanshu@trgtdigital.com>
2019-12-26 01:38:01 +0530

    add devcon.exe

--- devcon_win_driver.py
+++ devcon_win_driver.py
@@ -11,7 +11,7 @@ except ModuleNotFoundError:
     import devcon_win_copy
 import os
 
-# 123
+
 hardware_parm = 'find' # (status change requires admin privilege)
 hardware_id = devcon_win_copy.HP_TrueVision_HD() # (press space after 'devcon_win_copy.' for options)
 devcon_object = DevconClass()
