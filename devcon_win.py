8341b1e2f57b905c8518f2ff8a1e2941722a4043
Shivanshu Srivastava <shivanshu@trgtdigital.com>
2019-12-26 11:19:38 +0530

    pypi upload test

--- devcon_win.py
+++ devcon_win.py
@@ -1,39 +1,13 @@
 from __future__ import unicode_literals
 import os
 import sys
+import time
 import ctypes
 import inspect
 import subprocess
-from shutil import copyfile
 import getpass
-import time
-
-assert sys.version_info >= (2, 7), 'Python version should be at least 2.7'
-
-assert hasattr(sys, 'getwindowsversion'), "Operating system is not Windows"
-
-CURRENT_FILE_NAME = 'devcon_win.py'
-CURRENT_FILE_NAME_COPY = 'devcon_win_copy.py'
-
-to_be_replaced = 'to_be_replaced'
-if sys.version_info[0] == 3:
-    compat_text = ["      return subprocess.getoutput('devcon '+arg+  ' @\"'"+ to_be_replaced + ')',
-                   "   else:",
-                   "      if (ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, caller_script, None, 0)) !=42:"]
-else:
-    compat_text = ["      return subprocess.check_output('devcon '+arg+  ' @\"'" + to_be_replaced + ')',
-                   "   else:",
-                   "      if (ctypes.windll.shell32.ShellExecuteW(None, u'runas', unicode(sys.executable), unicode(caller_script), None, 0)) !=42:"]
-
-
-# refer https://stackoverflow.com/a/41930586
-def is_admin():
-    print('Authenticating with user: '+getpass.getuser())
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
-
+import logging
+from shutil import copyfile
 
 class DevconClass:
 
@@ -43,8 +17,8 @@ class DevconClass:
         except AttributeError:
             parsed_info = subprocess.check_output('devcon find ' + '@"*"').splitlines()
 
-        func_lst = ["print('Status:')",
-                    "print('')"]
+        func_lst = ["logging.debug('Status:')",
+                    "logging.debug('')"]
         for instance in parsed_info:
             instance = instance.split(':')
             if len(instance) > 1:
@@ -55,22 +29,22 @@ class DevconClass:
                 try:
                     func_lst.extend(
                         ["def " + instance[1] + "(arg='status'):",
-                         "   print('Argument: '+str(arg))",
+                         "   logging.debug('Argument: '+str(arg))",
                          "   if arg in ['status', 'find'] or is_admin(): ",
                          str(compat_text[0].replace('to_be_replaced', instance[0])),
                          str(compat_text[1]),
                          str(compat_text[2]),
-                         "         print('Authentication: failed')",
+                         "         logging.debug('Authentication: failed')",
                          "      else:",
-                         "         print('Authentication: successful')",
-                         "      print('')",
+                         "         logging.debug('Authentication: successful')",
+                         "      logging.debug('')",
                          "   time.sleep(2)",
                          "   return("+instance[1] + "('status'))"]
                     )
                 except SyntaxError:
                     continue
 
-        # print(func_lst)
+        # logging.debug(func_lst)
         func_lst.extend([
             "try:",
             "    os.remove('"+CURRENT_FILE_NAME_COPY+"')",
@@ -81,6 +55,35 @@ class DevconClass:
 
 
 if __name__ != '__main__':
+
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
+    assert sys.version_info >= (2, 7), 'Python version should be at least 2.7'
+
+    assert hasattr(sys, 'getwindowsversion'), "Operating system is not Windows"
+
+    CURRENT_FILE_NAME = 'devcon_win.py'
+    CURRENT_FILE_NAME_COPY = 'devcon_win_copy.py'
+
+    to_be_replaced = 'to_be_replaced'
+    if sys.version_info[0] == 3:
+        compat_text = ["      return subprocess.getoutput('devcon '+arg+  ' @\"'" + to_be_replaced + ')',
+                       "   else:",
+                       "      if (ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, caller_script, None, 0)) !=42:"]
+    else:
+        compat_text = ["      return subprocess.check_output('devcon '+arg+  ' @\"'" + to_be_replaced + ')',
+                       "   else:",
+                       "      if (ctypes.windll.shell32.ShellExecuteW(None, u'runas', unicode(sys.executable), unicode(caller_script), None, 0)) !=42:"]
+
+
+    # refer https://stackoverflow.com/a/41930586
+    def is_admin():
+        logging.debug('Authenticating with user: ' + getpass.getuser())
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+
 
     try:
         os.remove(CURRENT_FILE_NAME_COPY)
